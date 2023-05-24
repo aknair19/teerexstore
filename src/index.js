9bc332e729d83def18469838fc4c5c008fc51430
Abhishek Nair <abhisheknair1441@gmail.com>
2023-05-24 17:29:59 +0530

    added tailwind-config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import Homepage from './pages/Homepage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
